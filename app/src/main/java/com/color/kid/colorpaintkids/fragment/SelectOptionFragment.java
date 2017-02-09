757459b647a2f259f3af8870974f89e0be88c59a
TungDaiCa <nguyentung.fit.hut@gmail.com>
2017-02-09 21:00:50 +0700

    add quang cao

--- app/src/main/java/com/color/kid/colorpaintkids/fragment/SelectOptionFragment.java
+++ app/src/main/java/com/color/kid/colorpaintkids/fragment/SelectOptionFragment.java
@@ -5,7 +5,12 @@ import android.view.View;
 import com.color.kid.colorpaintkids.R;
 import com.color.kid.colorpaintkids.constance.Constants;
 import com.color.kid.colorpaintkids.util.FragmentUtil;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
+import butterknife.Bind;
 import butterknife.OnClick;
 
 /**
@@ -13,6 +18,10 @@ import butterknife.OnClick;
  */
 
 public class SelectOptionFragment extends BaseFragment {
+    private InterstitialAd mInterstitialAd;
+
+    @Bind(R.id.adView)
+    AdView adView;
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_select_option;
@@ -25,11 +34,85 @@ public class SelectOptionFragment extends BaseFragment {
 
     @Override
     protected void initData() {
+      showAdView();
+    }
 
+    public void showAdView(){
+        mInterstitialAd = new InterstitialAd(getActivity());
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+
+        /*mNewGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                } else {
+                    //Begin Game, continue with app
+                }
+            }
+        });*/
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+
+                //Begin Game, continue with app
+            }
+        });
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+        mInterstitialAd.loadAd(adRequest);
     }
 
     @OnClick(R.id.itemAnimal)
     void gotoListAminal(){
         FragmentUtil.pushFragment(getActivity(), SelectItemFragment.newInstance(Constants.AMINAL), null);
     }
+
+    @OnClick(R.id.itemCar)
+    void gotoListCar(){
+        FragmentUtil.pushFragment(getActivity(), SelectItemFragment.newInstance(Constants.AMINAL), null);
+    }
+
+    @OnClick(R.id.itemDinosaur)
+    void gotoListDinosaur(){
+        FragmentUtil.pushFragment(getActivity(), SelectItemFragment.newInstance(Constants.AMINAL), null);
+    }
+
+    @OnClick(R.id.itemDolphins)
+    void gotoListDolphins(){
+        FragmentUtil.pushFragment(getActivity(), SelectItemFragment.newInstance(Constants.AMINAL), null);
+    }
+
+    @OnClick(R.id.itemHorse)
+    void gotoListHorse(){
+        FragmentUtil.pushFragment(getActivity(), SelectItemFragment.newInstance(Constants.AMINAL), null);
+    }
+
+    @OnClick(R.id.itemHouse)
+    void gotoListHouse(){
+        FragmentUtil.pushFragment(getActivity(), SelectItemFragment.newInstance(Constants.AMINAL), null);
+    }
+
+    @OnClick(R.id.itemParty)
+    void gotoListParty(){
+        FragmentUtil.pushFragment(getActivity(), SelectItemFragment.newInstance(Constants.AMINAL), null);
+    }
+
+    @OnClick(R.id.itemSatan)
+    void gotoListSatan(){
+        FragmentUtil.pushFragment(getActivity(), SelectItemFragment.newInstance(Constants.AMINAL), null);
+    }
+
+    @OnClick(R.id.itemTrain)
+    void gotoListTrain(){
+        FragmentUtil.pushFragment(getActivity(), SelectItemFragment.newInstance(Constants.AMINAL), null);
+    }
+
+    @OnClick(R.id.itemMermaids)
+    void gotoListMermaids(){
+        FragmentUtil.pushFragment(getActivity(), SelectItemFragment.newInstance(Constants.AMINAL), null);
+    }
+
 }
