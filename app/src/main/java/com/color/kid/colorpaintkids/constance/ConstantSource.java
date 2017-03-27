45f95cde5fff23182203f490721f0f227b78b37a
Tung <nguyentung.fit.hut@gmail.com>
2017-03-27 16:25:48 +0700

    icon App

--- app/src/main/java/com/color/kid/colorpaintkids/constance/ConstantSource.java
+++ app/src/main/java/com/color/kid/colorpaintkids/constance/ConstantSource.java
@@ -20,7 +20,7 @@ public class ConstantSource {
             R.drawable.small_picture_61, R.drawable.small_picture_62, R.drawable.small_picture_63, R.drawable.small_picture_64, R.drawable.small_picture_65, R.drawable.small_picture_66,
     };
 
-    public static int[] listAminalDraw = {R.drawable.picture_1, R.drawable.picture_2, R.drawable.picture_3, R.drawable.picture_4, R.drawable.picture_5,
+    public static int[] listAminalDraw = {R.drawable.picture_1, R.drawable.picture_2, R.drawable.nhim, R.drawable.picture_3, R.drawable.picture_4, R.drawable.picture_5,
             R.drawable.picture_6, R.drawable.picture_7, R.drawable.picture_8, R.drawable.picture_9, R.drawable.picture_10, R.drawable.picture_11,
             R.drawable.picture_12, R.drawable.picture_13, R.drawable.picture_14, R.drawable.picture_15, R.drawable.picture_16, R.drawable.picture_17,
             R.drawable.picture_18, R.drawable.picture_19, R.drawable.picture_20, R.drawable.picture_21, R.drawable.picture_22, R.drawable.picture_23,
@@ -30,7 +30,7 @@ public class ConstantSource {
             R.drawable.picture_42, R.drawable.picture_43, R.drawable.picture_44, R.drawable.picture_45, R.drawable.picture_46, R.drawable.picture_47,
             R.drawable.picture_48, R.drawable.picture_49, R.drawable.picture_50, R.drawable.picture_51, R.drawable.picture_52, R.drawable.picture_53,
             R.drawable.picture_54, R.drawable.picture_56, R.drawable.picture_57, R.drawable.picture_58, R.drawable.picture_59, R.drawable.picture_60,
-            R.drawable.picture_61, R.drawable.picture_62, R.drawable.picture_63, R.drawable.picture_64, R.drawable.picture_65, R.drawable.picture_66,
+            R.drawable.picture_61, R.drawable.picture_62, R.drawable.picture_63, R.drawable.picture_64, R.drawable.picture_65, R.drawable.picture_66
     };
 
     public static int[] listCarDraw = {R.drawable.car_picture_1, R.drawable.car_picture_2, R.drawable.car_picture_3, R.drawable.car_picture_4, R.drawable.car_picture_5,
