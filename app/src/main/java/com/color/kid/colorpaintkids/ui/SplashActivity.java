2f6193e21487204ac3ee0f95258ed59e84bf4981
tung.nguyen1@ntq-solution.com.vn <tung@123456>
2018-11-21 14:03:49 +0700

    review code

--- app/src/main/java/com/color/kid/colorpaintkids/ui/SplashActivity.java
+++ app/src/main/java/com/color/kid/colorpaintkids/ui/SplashActivity.java
@@ -8,7 +8,10 @@ import android.support.v4.app.FragmentActivity;
 
 import com.color.kid.colorpaintkids.R;
 
+import org.jsoup.Jsoup;
+
 public class SplashActivity extends FragmentActivity {
+    String packageName;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +29,41 @@ public class SplashActivity extends FragmentActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                // TODO check update
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
             }
-        }, 2000);
+        }, 3000);
+    }
+
+    private boolean web_update(){
+        try {
+            packageName = "com.color.kid.colorpaintkids";
+            String curVersion = getApplication().getPackageManager().getPackageInfo(packageName, 0).versionName;
+            String newVersion = curVersion;
+            newVersion = Jsoup.connect("https://play.google.com/store/apps/details?id=" + packageName + "&hl=en")
+                    .timeout(30000)
+                    .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+                    .referrer("http://www.google.com")
+                    .get()
+                    .select("div[itemprop=softwareVersion]")
+                    .first()
+                    .ownText();
+            return (value(curVersion) < value(newVersion)) ? true : false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private long value(String string) {
+        string = string.trim();
+        if( string.contains( "." )){
+            final int index = string.lastIndexOf( "." );
+            return value( string.substring( 0, index ))* 100 + value( string.substring( index + 1 ));
+        }
+        else {
+            return Long.valueOf( string );
+        }
     }
 }
