4f342ee2f5b3b11aceb15abb761fa16347dc0f30
tungn <tungnv@runsystem.net>
2019-03-05 08:44:11 +0700

    change version app

--- app/src/main/java/com/color/kid/paint/ui/SplashActivity.java
+++ app/src/main/java/com/color/kid/paint/ui/SplashActivity.java
@@ -1,18 +1,25 @@
 package com.color.kid.paint.ui;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
 
 import com.color.kid.paint.R;
+import com.color.kid.paint.util.DebugLog;
+
+import java.security.MessageDigest;
 
 public class SplashActivity extends FragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        printHashKey();
     }
 
     @Override
@@ -27,6 +34,20 @@ public class SplashActivity extends FragmentActivity {
             // TODO check update
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();},3000);
+    }
+
+    public  void printHashKey() {
+        try {
+            final PackageInfo info = this.getPackageManager().getPackageInfo(this.getPackageName(), PackageManager.GET_SIGNATURES);
+            for (android.content.pm.Signature signature : info.signatures) {
+                final MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                final String hashKey = new String(Base64.encode(md.digest(), 0));
+                DebugLog.e("AppLog key:" + hashKey + "=");
+            }
+        } catch (Exception e) {
+            DebugLog.e("AppLog+error:" + e);
+        }
     }
 
 }
